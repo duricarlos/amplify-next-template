2ad40ff5f6c90301a931e910e1f2ba9bb0133b9c
Carlos Duri <me@carlosduri.com>
2024-12-06 00:09:33 -0500

    Add navigation link to a test page in App component for improved user experience and quick access to todos

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import Link from "next/link";
 
 // Amplify.configure(outputs);
 
@@ -27,6 +28,9 @@ export default async function App() {
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
         </a>
+        <Link href="/test">
+          <button>Go to test</button>
+        </Link>
       </div>
     </main>
   );
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+  return (
+    <main>
+      <h1>My todos</h1>
+    </main>
+  );
+}
